28094ca178f627d80416e71caba2fe12223e61ff
Mihail Georgiev <mihail.georgiev@brandwatch.com>
2025-11-11 19:47:27 +0200

    added js

--- myscript.js
+++ myscript.js
@@ -1,4 +1,55 @@
 
 
-function x() {
+function revealLoginForm(event) {
+    const loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'block';
+    console.log(event);
 }
+
+function hideLoginForm(event) {
+    const loginForm = document.getElementById('login-form');
+    loginForm.style.display = 'none';
+}
+
+function selectListElement(event) {
+
+    if (selectedElements.has(event.target)) {
+        event.target.classList.remove('selected');
+        selectedElements.delete(event.target);
+        return;
+    }
+    event.target.classList.add('selected');
+    selectedElements.add(event.target);
+}
+
+function selectListElementV2(event) {
+
+    if (event.target.getAttribute('data-selected') === 'true') {
+        event.target.removeAttribute('data-selected');
+        event.target.classList.remove('selected');
+        return;
+    }
+    event.target.classList.add('selected');
+    event.target.setAttribute('data-selected', 'true');
+}
+
+function getSelectedElements() {
+    return selectedElements;
+}
+
+function getSelectedElementsV2() {
+    return document.querySelectorAll('#container div[data-selected="true"]');
+}
+
+
+const loginButton = document.getElementById('login-button');
+loginButton.addEventListener('click', revealLoginForm);
+
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', hideLoginForm);
+
+let selectedElements = new Set();
+
+document.querySelectorAll('#container div').forEach(element => {
+    element.addEventListener('click', selectListElementV2);
+});
